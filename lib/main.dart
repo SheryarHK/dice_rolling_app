a38c7ed5b0a58744c96068342270062aa5627e85
sheryarhk <sheryarhassankhan@gmail.com>
2019-07-14 13:56:31 +0500

    added stateless wigdet

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Dice Rolling'),
+  return runApp(
+    myApp(),
+  );
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Dice Rolling'),
+        ),
       ),
-    ),
-  ));
+    ));
+  }
 }
