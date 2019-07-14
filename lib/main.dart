215b3e0b9c70547e4040cfa8cc0b3b3cabaa5e42
sheryarhk <sheryarhassankhan@gmail.com>
2019-07-14 14:39:36 +0500

    added images and centered them. Also added paddings.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    myApp(),
-  );
+  return runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.red,
+    appBar: AppBar(
+      backgroundColor: Colors.red,
+      title: Text('Dice Rolling'),
+    ),
+    body: DiceApp(),
+  )));
 }
 
-class myApp extends StatelessWidget {
+class DiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('Dice Rolling'),
-        ),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
